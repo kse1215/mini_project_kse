f0781f3e9e08a9b91a7916825bce906cf60fb994
kse1215 <dev.kseoeun1215@gmail.com>
2025-09-09 17:59:34 +0900

    지구수비대 작업 시작

--- js/kse.js
+++ js/kse.js
@@ -8,4 +8,27 @@ window.addEventListener("load", function () {
       el.classList.add("active");
     };
   });
+
+  // 지구수비대
+
+  // 스와이퍼 ex
+  var swiper = new Swiper(".earths_protector_swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true, // ✅ 가운데 정렬
+    slidesPerView: 2, // ✅ 3장만 보이게
+    spaceBetween: 30,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 200,
+      modifier: 2.5,
+      slideShadows: true,
+    },
+    loop: true, // 무한 반복
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
 });
